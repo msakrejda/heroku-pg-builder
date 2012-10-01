71c432ccce533e6836d3cffd13a154fc94161406
Maciek Sakrejda <maciek@heroku.com>
2012-09-30 21:56:34 -0700

    Adding some informational output to help with debugging.

--- build-pgsql.sh
+++ build-pgsql.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo -n "Building PostgreSQL, version "
+sed -n -r 's/#define PG_VERSION "(.*)"/\1/p' src/include/pg_config.h
+
 ./configure --prefix=/app/vendor --with-openssl  
 make && make install
